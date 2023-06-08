8262b3ca9364c00cc41d178f8d3954f1ec9d2af7
safawat hossain <sifakatali@gmail.com>
2023-06-08 23:44:52 +0600

    huskey

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,12 @@
-
-import express, { Application,Request, Response} from 'express'
-import cors from 'cors'
-const app : Application = express()
-const port = 3000
+import express, { Application, Request, Response } from 'express'
+const app: Application = express()
 
 //parser
 
 app.use(express.json())
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }))
 
-app.get('/', (req: Request, res:Response) => {
+app.get('/', (req: Request, res: Response) => {
   res.send('working successfully')
 })
 
