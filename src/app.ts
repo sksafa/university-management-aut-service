79cd82461aa05c341c944a559ad118d9fd67a2bc
safawat hossain <sifakatali@gmail.com>
2023-06-09 20:52:30 +0600

    .vscode ade

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,14 @@
 import express, { Application, Request, Response } from 'express'
+import usersRouter from './app/modules/user/users.route'
 const app: Application = express()
 
 //parser
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+
+//application routes
+app.use('/api/v1/users', usersRouter)
 
 app.get('/', (req: Request, res: Response) => {
   res.send('working successfully')
